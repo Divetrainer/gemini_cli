7acceb7c101ef0dbc3570f7b1616ef9c490afd82
Divetrainer <ian@divelab.tech>
2026-04-14 00:12:40 -0700

    finished and valdiated functions for ai overlord

--- functions/write_file.py
+++ functions/write_file.py
@@ -8,17 +8,20 @@ def write_file(working_directory, file_path, content):
 		working_dir_abs = os.path.abspath(working_directory)
 		target_file = os.path.normpath(os.path.join(working_dir_abs, file_path))
 
+#		print(target_file)
+
 		if os.path.commonpath([working_dir_abs, target_file]) != working_dir_abs:
 			return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+
+		os.makedirs(os.path.dirname(target_file), exist_ok=True)
 
 		if os.path.isdir(target_file):
 			return f'Error: Cannot write to "{file_path}" as it is a directory'
 
-		os.makedirs(file_path, exist_ok=True)
-
 		with open(target_file, "w") as f:
-			content
-			return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+			f.write(content)
+
+		return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
 
 
 	except Exception as e:
