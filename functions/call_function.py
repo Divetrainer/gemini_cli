150ca801ef9a8f675312511f49a678caf2cfbefb
Divetrainer <ian@divelab.tech>
2026-04-21 20:38:27 -0700

    adjust call function to allow llm to utilize other functions

--- functions/call_function.py
+++ functions/call_function.py
@@ -13,16 +13,42 @@ available_functions = types.Tool(
 		schema_write_file],
 )
 
-function_map = {
-	"get_files_info":get_files_info,
-	"get_file_info":get_file_info,
-	"run_python_file":run_python_file,
-	"write_file":write_file,
-}
-
 
 def call_function(function_call, verbose=False):
 	if verbose:
 		print(f"Calling function: {function_call.name}({function_call.args})")
 	else:
 		print(f" - Calling function: {function_call.name}")
+
+	function_map = {
+		"get_files_info":get_files_info,
+		"get_file_info":get_file_info,
+		"run_python_file":run_python_file,
+		"write_file":write_file,
+	}
+	function_name = function_call.name or ""
+	if function_name is None:
+		return types.Content(
+			role="tool",
+			parts=[
+				types.Part.from_function_response(
+					name=function_name,
+					response={"error": f"Unknown function: {function_name}"},
+				)
+			],
+		)
+	args = dict(function_call.args) if function_call.args else{}
+
+	args["working_directory"] = "./calculator"
+
+	function_result = function_map[function_name](**some_args)
+	return types.Content(
+		role="tool",
+		parts=[
+			types.Part.from_function_reponse(
+				name=function_name,
+				response={"result": function_result},
+			)
+		],
+	)
+
